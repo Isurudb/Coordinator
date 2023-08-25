00589ae9217864d4ab59694be017eccf2c3eb87c
Isuru Basnayake <isurudb@gmail.com>
2023-08-25 01:50:46 -0700

    Simulation issue for multirobot fixed

--- include/coordinator/primary_tests.hpp
+++ include/coordinator/primary_tests.hpp
@@ -16,8 +16,8 @@ void PrimaryNodelet::RunTest0(ros::NodeHandle *nh){
     
     ros::Duration(5.0).sleep();
     ROS_INFO("Undocking the Astrobee ");
-    NODELET_INFO_STREAM("Calling " << undock_command);
-    system_ret = system(undock_command.c_str());
+    //NODELET_INFO_STREAM("Calling " << undock_command);
+    //system_ret = system(undock_command.c_str());
 
     if(system_ret != 0){
         NODELET_ERROR_STREAM("[PRIMARY/DMPC] Failed to Launch DMPC nodes.");
